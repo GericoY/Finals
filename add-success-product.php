ae54fb3901d293d2dae5596f5f3bc123af1f2105
GericoY <gericoyutuc@gmail.com>
2022-12-11 15:16:27 +0800

    Fix Add Record

--- add-success-product.php
+++ add-success-product.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Person</title>
-</head>
-<body>
-    <h1>Add Persons</h1>
-    <p>Record Succesfully Added!</p>
-    <a href="persons.php">Add another product</a>
-</body>
-</html>
+<?php require_once("header.php") ?>
+    <div class="container-fluid">
+        <div class="row">
+        <?php require_once("nav.php") ?>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">Add Products</h1>
+            </div>
+            <p>Record Successfully Added!</p>
+            <a href="products.php" class="btn btn-primary">Ok</a>
+<?php require_once("footer.php") ?>
--- products.php
+++ products.php
@@ -5,37 +5,63 @@
     require_once("header.php"); 
     require_once("functions.php");
 
-    if(isset($_POST['add'])){
-        $prodName = sanitizeInput($con, $_POST['prodName']);
-        $prodDesc = sanitizeInput($con, $_POST['prodDesc']);
-        $prodPrice = sanitizeInput($con, $_POST['prodPrice']);
-        $photo1 = $_POST['photo1'];
-        $photo2 = $_POST['photo2'];
+    if(isset($_POST['btnAdd']) && isset($_FILES['photo1']) && isset($_FILES['photo2'])){
+        $con = openConnection();
+        $name = sanitizeInput($con, $_POST['prodName']);
+        $description = sanitizeInput($con, $_POST['prodDesc']);
+        $price = $_POST['prodPrice'];
 
         $err = [];
 
-        if(empty($prodName))
-            $err = "Product Name is required!";
-        if(empty($prodDesc))
-            $err = "Product Description is required!";
-        if(empty($prodPrice))
-            $err = "Product Price is required!";
+        $fileName = $_FILES['photo1']['name'];
+        $fileSize = $_FILES['photo1']['size'];
+        $fileTemp = $_FILES['photo1']['tmp_name'];
+        $fileType = $_FILES['photo1']['type'];
+
+        $fileExtTemp = explode('.', $fileName);
+        $fileExt = strtolower(end( $fileExtTemp));
+
+        $fileNameTwo = $_FILES['photo2']['name'];
+        $fileSizeTwo = $_FILES['photo2']['size'];
+        $fileTempTwo = $_FILES['photo2']['tmp_name'];
+        $fileTypeTwo = $_FILES['photo2']['type'];
+
+        $fileExtTempTwo = explode('.', $fileNameTwo);
+        $fileExtTwo = strtolower(end( $fileExtTempTwo));
+
+        $allowed = array('jpeg', 'jpg', 'png');
+
+    
+        $uploadDir = 'uploads/' . $fileName;
+        $uploadDirTwo = 'uploads/' . $fileNameTwo;
+
+        if (in_array($fileExt, $allowed) === false && in_array($fileExtTwo, $allowed) === false)
+            $err[] = "Extension file is not allowed";
+        if ($fileSize > 5000000 && $fileSizeTwo > 5000000 )
+            $err[] = "File Should be 5mb Maximum";
+        if(empty($name))
+            $err[] = "Last name is required!";
+        if(empty($description))
+            $err[] = "Description is required!";
+        if(empty($price))
+            $err[] = "Price is required!";
 
         if(empty($err)){
-            $con = openConnection();
+                $strSql ="
+                        INSERT INTO tbl_products(name, description, price, photo1, photo2)
+                        VALUES('$name', '$description', '$price', '$fileName', '$fileNameTwo')
+                    ";
 
-            $strSql = "
-                    INSERT INTO tbl_products(name, description, price)
-                    VALUES ('$prodName', '$prodDesc', '$prodPrice')
-            ";
-
-            if(mysqli_query($con, $strSql))
-                header('add-success-product');
-            else
-                echo 'ERROR: Failed to insert record!';
-            
-            closeConnection($con);
+                if(mysqli_query($con, $strSql)){
+                    move_uploaded_file($fileTemp , $uploadDir);
+                    move_uploaded_file($fileTempTwo , $uploadDirTwo);
+                    header("location:add-success-product.php ");
+                }          
+                else
+                    echo 'Error: Failed to insert record';
+           
         }
+        closeConnection($con); 
     }
 ?>
 
@@ -48,87 +74,83 @@
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2">Add Products</h1>
                     </div>
+                        <form method="post" enctype="multipart/form-data">
+                            <div class="form-group row">
+                                <label for="prodName" class="col-sm-2 col-form-label">Name</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="prodName" id="prodName" required>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="prodDesc" class="col-sm-2 col-form-label">Description</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="prodDesc" id="prodDesc" required>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="prodPrice" class="col-sm-2 col-form-label">Price</label>
+                                <div class="col-sm-10">
+                                    <input type="number" class="form-control" name="prodPrice" id="prodPrice" required>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="photo1" class="col-sm-2 col-form-label"> Photo 1</label>
+                                <div class="col-sm-10">
+                                    <input type="file" class="form-control" name="photo1" id="photo1">
+                                </div>
+                            </div>
+                                <div class="form-group row">
+                                <label for="photo2" class="col-sm-2 col-form-label"> Photo 2</label>
+                                <div class="col-sm-10">
+                                    <input type="file" class="form-control" name="photo2" id="photo2" required>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-sm-10">
+                                    <button type="submit" name="btnAdd" class="btn btn-primary  ">Add New Record</button>
+                                </div>
+                            </div>
+                        </form>
+                        <br><br>
 
-                    <form method="post">
-                        <div class="form-group row">
-                            <label for="prodName" class="col-sm-2 col-form-label text-right">Product Name: <span class="text-danger">*</span></label>
-                            <div class="col-sm-10">
-                                <input type="text" name="prodName" id="prodName" class="form-control" required autofocus>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="prodDesc" class="col-sm-2 col-form-label text-right">Product Description: <span class="text-danger">*</span></label>
-                            <div class="col-sm-10">
-                                <input type="text" name="prodDesc" id="prodDesc" class="form-control" required>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="prodPrice" class="col-sm-2 col-form-label text-right">Product Price: <span class="text-danger">*</span></label>
-                            <div class="col-sm-10">
-                                <input type="text" name="prodPrice" id="prodPrice" class="form-control" required>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="photo1" class="col-sm-2 col-form-label text-right">Select Photo 1: <span class="text-danger">*</span></label>
-                            <div class="col-sm-10">
-                                <input type="file" name="photo1" id="photo1" class="form-control" >
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="photo2" class="col-sm-2 col-form-label text-right">Select Photo 1: <span class="text-danger">*</span></label>
-                            <div class="col-sm-10">
-                                <input type="file" name="photo2" id="photo2" class="form-control" >
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="offset-sm-2 col-sm-10">
-                                <button type="submit" name="add" class="btn btn-primary">Add</button>
-                            </div>
-                        </div>
-                    </form>
+                    <h3> <i class="fa fa-table"></i>Products List</h1>
+                    <div class="table-responsive">
+                        <table class="table table-striped table-sm">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Description</th>
+                                    <th>Price</th>
+                                    <th>Photo</th>
+                                    <th colspan="2" >Options</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    $con = openConnection();
+                                    $strSql = "SELECT * FROM tbl_products";
+                                    $recProducts = getRecord($con, $strSql);
 
-                <h2>Products</h2>    
-                <div class="table-responsive">
-                    <table class="table table-striped table-sm">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Description</th>
-                                <th>price</th>
-                                <th>photo1</th>
-                                <th>photo2</th>
-                                <th colspan="2">Options</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            <?php
-                                $con = openConnection();
-                                $strSql = "SELECT * FROM tbl_products";
-                                $recPersons = getRecord($con, $strSql);
-
-                                if(!empty($recPersons)){
-                                    foreach ($recPersons as $key => $value) {
-                                            echo '<tr>';
-                                                echo '<td>' . $value['id'] . '</td>';
-                                                echo '<td>' . $value['name'] . '</td>';
-                                                echo '<td>' . $value['description'] . '</td>';
-                                                echo '<td>' . $value['price'] . '</td>';
-                                                echo '<td>' . $value['photo1'] . '</td>';
-                                                echo '<td>' . $value['photo2'] . '</td>';
-                                                echo '<td>
-                                                    <a href="update-person.php?k='. $value[$key] .'" class="btn btn-success"></a>
-                                                    <a href="delete-person.php?k='. $value[$key] .'" class="btn btn-danger"></a>
-                                                </td>';
-                                            echo '</tr>';
+                                    if(!empty($recProducts)){
+                                        foreach ($recProducts as $key => $value) {
+                                        echo '<tr>';
+                                            echo '<td>' . $value['name'] .'</td>';
+                                            echo '<td>' . $value['description'] .'</td>';
+                                            echo '<td>' . $value['price'] .'</td>';
+                                            echo '<td><img src="uploads/'. $value['photo1'] .'" style="height: 50px;"></td>';
+                                            echo '<td>'; 
+                                                echo '<a href="edit-products.php?k=' . $value['id'] .'" class="btn btn-success">Edit</a> ';
+                                                echo '<a href="delete-products.php?k=' . $value['id'] .'" class="btn btn-danger">Remove</a>';
+                                            echo '</td>';
+                                        echo '</tr>';                       
+                                        }
                                     }
-                                }
-                                else{
-
-                                }
-
-                                closeConnection($con);
+                                    else{
+                                        echo '<tr>';
+                                            echo '<td colspan="5" class="text-center"> No Records found</td>';
+                                        echo '</tr>';
+                                    }
+                                closeConnection($con);                  
                             ?>
                         </tbody>
                     </table>
